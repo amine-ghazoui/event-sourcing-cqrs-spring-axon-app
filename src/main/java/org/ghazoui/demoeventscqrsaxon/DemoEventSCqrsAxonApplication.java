0c0d1f426336dd0568e6cda636bddc0893b8b138
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-12-05 12:10:19 +0100

    First commit

---
+++ src/main/java/org/ghazoui/demoeventscqrsaxon/DemoEventSCqrsAxonApplication.java
@@ -0,0 +1,13 @@
+package org.ghazoui.demoeventscqrsaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEventSCqrsAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEventSCqrsAxonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ghazoui/demoeventscqrsaxon/DemoEventSCqrsAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ghazoui.demoeventscqrsaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEventSCqrsAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
